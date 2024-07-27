c2d75a98d8c34618d13f423ddbca7b1ec00cf6ae
Hoyun Jung <jhy9094@gmail.com>
2024-07-27 19:01:03 +0900

    섹션 4 개념 정리

--- basic/src/main/java/study/week1/section4/hoyunjung/Main.java
+++ basic/src/main/java/study/week1/section4/hoyunjung/Main.java
@@ -1,4 +1,9 @@
 package study.week1.section4.hoyunjung;
 
 public class Main {
+    public static void main(String[] args) {
+        // 기본 생성자 사용
+        // 객체 생성(힙 할당) -> 생성자 호출
+        new Constructor();
+    }
 }
