9a47e95e5f6f9b73029d1dc6bf50f36304992571
Hoyun Jung <jhy9094@gmail.com>
2024-07-27 20:26:57 +0900

    주석 수정

--- basic/src/main/java/study/week1/section4/hoyunjung/Main.java
+++ basic/src/main/java/study/week1/section4/hoyunjung/Main.java
@@ -2,8 +2,7 @@ package study.week1.section4.hoyunjung;
 
 public class Main {
     public static void main(String[] args) {
-        // 기본 생성자 사용
-        // 객체 생성(힙 할당) -> 생성자 호출
-        new Constructor();
+        // 순서: 객체 생성(힙 할당) -> 생성자 호출
+        new Constructor();  // 기본 생성자
     }
 }
