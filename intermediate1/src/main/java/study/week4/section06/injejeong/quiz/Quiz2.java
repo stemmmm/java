32c4b067d7dbbb8f54fc1b21fc00f9d505f0f01c
jij0128 <daniel2010298@gmail.com>
2024-09-01 23:51:58 -0500

    날짜와 시간 문제 풀이 추가

---
+++ intermediate1/src/main/java/study/week4/section06/injejeong/quiz/Quiz2.java
@@ -0,0 +1,17 @@
+package study.week4.section06.injejeong.quiz;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+        // 2024년 1월 1일 부터 2주 간격으로 5번 반복하여 날짜를 출력
+        LocalDate localDate = LocalDate.of(2024, 1, 1);
+        System.out.println(localDate);
+
+        for (int i = 0; i < 5; i++) {
+            localDate = localDate.plusWeeks(2);
+            System.out.println(localDate);
+        }
+    }
+}
